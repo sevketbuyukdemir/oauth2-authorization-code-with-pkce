ed3c0d7d18bb705eb7d49838ba9eb418381b2a49
sevketbuyukdemir <sevket.buyukdemir@gmail.com>
2025-12-17 15:40:33 +0300

    AUTHZ-CODE-PKCE-2 - Resource server implementation.

---
+++ resource-server/src/main/java/com/sevketbuyukdemir/resource_server/controller/AdminResourceController.java
@@ -0,0 +1,24 @@
+package com.sevketbuyukdemir.resource_server.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/admin-resources", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequiredArgsConstructor
+public class AdminResourceController {
+
+    @GetMapping("/secret-resource")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<String> first(Authentication authentication, HttpServletRequest request) {
+        return new ResponseEntity<>("SECRET ADMIN RESOURCE", HttpStatus.OK);
+    }
+}
---
+++ resource-server/src/main/java/com/sevketbuyukdemir/resource_server/controller/UserResourceController.java
@@ -0,0 +1,23 @@
+package com.sevketbuyukdemir.resource_server.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/user-resources", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequiredArgsConstructor
+public class UserResourceController {
+
+    @GetMapping("/secret-resource")
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<String> first(Authentication authentication, HttpServletRequest request) {
+        return new ResponseEntity<>("SECRET USER RESOURCE", HttpStatus.OK);
+    }
+
+}
